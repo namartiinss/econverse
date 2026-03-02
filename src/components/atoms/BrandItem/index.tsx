bfebce5e16c42b7cd353cb18a846fef52cf9a6f2
Nathalia Martins <nathaliamartins0708@gmail.com>
2026-03-02 20:41:54 -0300

    fix: Correção de estrutura em brands

--- src/components/atoms/BrandItem/index.tsx
+++ src/components/atoms/BrandItem/index.tsx
@@ -2,12 +2,13 @@ import styles from './index.module.scss'
 
 interface BrandItemProps {
   img?: string;
+  alt?: string;
 }
 
-function BrandItem({img}: BrandItemProps) {
+function BrandItem({img, alt}: BrandItemProps) {
   return (
     <div className={styles.circleBrand}>
-        <img className={styles.sizeBrand} src={img} alt="" />
+        <img className={styles.sizeBrand} src={img} alt={alt || "Brand"} />
     </div>
   )
 }
--- src/components/templates/Brands/index.tsx
+++ src/components/templates/Brands/index.tsx
@@ -1,17 +1,32 @@
 import styles from './index.module.scss'
 import BrandItem from '../../atoms/BrandItem'
-
+import LogoEconVerse from '../../../assets/logotipoEconverse.png'
 function Brands() {
+  const brands = [
+    {
+      img: LogoEconVerse, alt: 'Econverse', id: 1
+    },
+    {
+      img: LogoEconVerse, alt: 'Econverse', id: 2
+    },
+    {
+      img: LogoEconVerse, alt: 'Econverse', id: 3
+    },
+    {
+      img: LogoEconVerse, alt: 'Econverse', id: 4
+    },
+    {
+      img: LogoEconVerse, alt: 'Econverse', id: 5
+    },
+  ]
   return (
     <section className={styles.containerBrand}>
-        <h2>Navegue por marcas</h2>
-        <div className={styles.contentBrand}>
-            <BrandItem />
-            <BrandItem />
-            <BrandItem />
-            <BrandItem />
-            <BrandItem />
-        </div>
+      <h2>Navegue por marcas</h2>
+      <div className={styles.contentBrand}>
+        {brands.map(brand => (
+          <BrandItem key={brand.id} img={brand.img} alt={brand.alt} />
+        ))}
+      </div>
     </section>
   )
 }
