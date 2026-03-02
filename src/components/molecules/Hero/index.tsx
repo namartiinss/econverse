40a8e3094d19f22f8f20ec9bc04eeb2f25d5fedc
Nathalia Martins <nathaliamartins0708@gmail.com>
2026-03-02 19:58:43 -0300

    fix: Desenvolvimento hero

--- src/components/molecules/Hero/index.tsx
+++ src/components/molecules/Hero/index.tsx
@@ -3,13 +3,13 @@ import styles from './index.module.scss'
 
 function Hero() {
     return (
-        <section>
+        <section className={styles.bannerHero}>
             <div className={styles.containerHero}>
                 <div className={styles.contentHero}>
                     <div className={styles.contentText}>
                         <h1>Venha conhecer nossas promoções</h1>
                         <h2><span>50% Off</span> nos produtos </h2>
-                        <Button href='#' children="Ver produto" />
+                        <Button href='#produtos' children="Ver produto" />
                     </div>
                 </div>
             </div>
