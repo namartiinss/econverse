cb8b66150a5c6b79e48a414ee7c74319629ac4b7
Nathalia Martins <nathaliamartins0708@gmail.com>
2026-03-02 20:59:08 -0300

    fix: Correção em newsletter e footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,8 @@
-import Filters from "./components/molecules/Filters"
-import Hero from "./components/molecules/Hero"
-import Brands from "./components/templates/Brands"
-import Footer from "./components/templates/Footer"
-import Header from "./components/templates/Header"
-import Informations from "./components/templates/Informations"
-import Newsletter from "./components/templates/Newsletter"
-import Products from "./components/templates/Products"
+import Home from "./components/page/home"
 
 function App() {
   return (
-    <>
-      <Header />
-      {/* <Hero />
-      <Filters />
-      <Products />
-      <Informations />
-      <Brands />
-      <Products />
-      <Newsletter />
-      <Footer /> */}
-    </>
+     <Home />
   )
 }
 export default App
---
+++ src/components/page/home/index.tsx
@@ -0,0 +1,27 @@
+import Header from '../../templates/Header'
+import Hero from '../../molecules/Hero'
+import Filters from '../../molecules/Filters'
+import Products from '../../templates/Products'
+import Informations from '../../templates/Informations'
+import Brands from '../../templates/Brands'
+import Newsletter from '../../templates/Newsletter'
+import Footer from '../../templates/Footer'
+
+function Home() {
+    return (
+        <>
+            <Header />
+            <Hero />
+            <Filters />
+            <Products hasCategories={true} />
+            <Informations />
+            <Products hasCategories={false} />
+            <Brands />
+            <Products hasCategories={false} />
+            <Newsletter />
+            <Footer />
+        </>
+    )
+}
+
+export default Home
