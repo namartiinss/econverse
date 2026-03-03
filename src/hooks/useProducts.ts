eee9717c4ded9176dea66a6e5a1d6d5adc547500
Nathalia Martins <nathaliamartins0708@gmail.com>
2026-03-03 07:13:41 -0300

    fix: Correção de baseURL para fetch em produção

--- src/hooks/useProducts.ts
+++ src/hooks/useProducts.ts
@@ -6,12 +6,15 @@ interface Product {
     photo: string
     price: number
 }
+const BASE_URL = import.meta.env.PROD
+  ? 'https://app.econverse.com.br'
+  : '/api'
 
 export function useProducts() {
     const [products, setProducts] = useState<Product[]>([])
 
     useEffect(() => {
-        fetch('/api/teste-front-end/junior/tecnologia/lista-produtos/produtos.json')
+        fetch(`${BASE_URL}/teste-front-end/junior/tecnologia/lista-produtos/produtos.json`)
             .then(res => res.json())
             .then(data => setProducts(data.products))
     }, [])
