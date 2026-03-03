3ede00ae0f7a9bc00b1230fa64dd6e6e914e8cb1
Nathalia Martins <nathaliamartins0708@gmail.com>
2026-03-02 21:05:51 -0300

    fix: Correção em carrossel adição de arrows

--- src/components/organisms/SwiperProducts/index.tsx
+++ src/components/organisms/SwiperProducts/index.tsx
@@ -1,16 +1,26 @@
 import styles from "./index.module.scss"
 import { Swiper, SwiperSlide } from 'swiper/react'
+import { useRef } from 'react'
+import type { SwiperRef } from 'swiper/react'
 import ProductCard from '../../molecules/ProductCard'
 import 'swiper/css'
-
 import { useProducts } from '../../../hooks/useProducts'
-
+import arrowRight from '../../../assets/arrowRight.svg'
+import arrowLeft from '../../../assets/arrowLeft.svg'
 const Carousel = () => {
   const { products } = useProducts()
+  const swiperRef = useRef<SwiperRef>(null)
 
   return (
     <div className={styles.containerCard}>
-      <Swiper spaceBetween={18} slidesPerView={4}>
+      <button className={styles.prevButton} onClick={() => swiperRef.current?.swiper.slidePrev()}>
+        <img src={arrowLeft} alt="Previous" />
+      </button>
+      <button className={styles.nextButton} onClick={() => swiperRef.current?.swiper.slideNext()}>
+        <img src={arrowRight} alt="Next" />
+      </button>
+
+      <Swiper ref={swiperRef} spaceBetween={18} slidesPerView={4}>
         {products.map((product, index) => (
           <SwiperSlide key={index}>
             <ProductCard
