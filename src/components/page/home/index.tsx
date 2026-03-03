75479f7ad4f8888e0eda2e73ea8ce682f43c5a6d
Nathalia Martins <nathaliamartins0708@gmail.com>
2026-03-03 07:06:27 -0300

    fix: Update em responsivo de containers para desktop e mobile

--- src/components/page/home/index.tsx
+++ src/components/page/home/index.tsx
@@ -17,7 +17,7 @@ function Home() {
             <Informations />
             <Products hasCategories={false} />
             <Brands />
-            <Products hasCategories={false} /> 
+            <Products hasCategories={false} />
             <Newsletter />
             <Footer />
         </>
